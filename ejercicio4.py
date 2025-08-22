791c978862527a51abebf518be8d3b3ff3982c47
Alegbarrio <a01564273@tec.mx>
2025-08-22 16:43:40 +0000

    c

---
+++ ejercicio4.py
@@ -0,0 +1,16 @@
+
+print("Ingresa un número entero de champions")
+
+champs=abs(int(input("Dame el número: ")))
+
+match champs:
+    case 15:
+        print("Real Madrid")
+    case 7:
+        print("AC Milán")
+    case 6:
+        print("Liverpool y Bayern")
+    case 5:
+        print("Barcelona")
+
+
