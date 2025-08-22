165be02f9bd5f19194e044f3659280199647c746
Alegbarrio <a01564273@tec.mx>
2025-08-22 15:43:09 +0000

    ale

---
+++ ejercicio1.py
@@ -0,0 +1,22 @@
+numero=int(input("Dame un numero: "))
+
+if(numero>=0):
+    print("Positivo")
+    if(numero%2==0):
+        print("Par")
+    else:
+        print("Impar")
+else:
+    print("Negativo")
+    if(numero%2==0):
+        print("Par")
+    else:
+        print("Impar")
+        
+              
+
+
+
+
+        
+        
